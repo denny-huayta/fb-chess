da808163fcd59ff862aaf89a84279ca50910ba6b
dhuayta <denny_ha@hotmail.com>
2012-10-16 18:44:38 -0500

    refactoring

--- lib/chessmaster.rb
+++ lib/chessmaster.rb
@@ -7,16 +7,16 @@ require 'mongo'
 require 'json'
 require 'mongo_mapper'
 require 'uri'
-#require 'uuid'
 
 require_relative 'game'
 require_relative 'chessboard'
+require_relative 'chessmasterbo'
 
 # register your app at facebook to get those infos
 APP_ID = 386008508137576 # your app id
 APP_CODE = '1fcec4d0014d0dd766c12bd54a65e27b' # your app code
-SITE_URL = 'http://fb-chess.herokuapp.com/' # your app site url
-#SITE_URL = 'http://localhost:9292/'
+#SITE_URL = 'http://fb-chess.herokuapp.com/' # your app site url
+SITE_URL = 'http://localhost:9292/'
 
 class CHESSMASTER < Sinatra::Application
 	
@@ -25,41 +25,30 @@ class CHESSMASTER < Sinatra::Application
 	set :root, APP_ROOT
 	enable :sessions
 
-	# Mongo Mapper Connection
-	#MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5, :timeout => 5)
-	#MongoMapper.database = 'mydb'
-	#MongoMapper.database.authenticate('','')
+	chessmasterbo = Chessmasterbo.new
 
-	db = URI.parse(ENV['MONGOHQ_URL']) 	
- 	MongoMapper.connection  = Mongo::Connection.new(db.host, db.port)
- 	MongoMapper.database = 'app8043150'
- 	MongoMapper.database.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+	# Mongo Mapper Connection
+	MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5, :timeout => 5)
+	MongoMapper.database = 'mydb'
+
+	#db = URI.parse(ENV['MONGOHQ_URL']) 	
+ 	#MongoMapper.connection  = Mongo::Connection.new(db.host, db.port)
+ 	#MongoMapper.database = 'app8043150'
+ 	#MongoMapper.database.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
 
 	get '/' do
 
-		# TO BE DELETED
-		#session['userId']	= '123456789'
-		#session['name']		= 'Denny Huayta'
-		#session['username']	= 'dhuayta'
-		#session['email']	= 'denny_ha@hotmail.com'
-
-
 		if session['access_token']
-		 	#'You are logged in! <a href="/logout">Logout</a>'
-			# do some stuff with facebook here
-			# for example:
-			#@graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-			# publish to your wall (if you have the permissions)
-			#@graph.put_wall_post("Sign up from Chessmaster!" + Time.now.to_s )
-			# or publish to someone else (if you have the permissions too ;) )
-			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
 
 			@api = Koala::Facebook::API.new(session[:access_token])
 			@user_info = @api.get_object("me")
-			session['userId']	= @user_info['id']
-			session['name']		= @user_info['name']
-			session['username']	= @user_info['username']
-			session['email']	= @user_info['email']
+
+			session['userInfo'] = @user_info
+
+			#session['userId']	= @user_info['id']
+			#session['name']		= @user_info['name']
+			#session['username']	= @user_info['username']
+			#session['email']	= @user_info['email']
 
 			erb :index
 		else
@@ -67,12 +56,9 @@ class CHESSMASTER < Sinatra::Application
 		end		
 	end
 
-	get '/login' do
-		# generate a new oauth object with your app data and your callback url
+	get '/login' do		
 		session['oauth'] = Facebook::OAuth.new(APP_ID, APP_CODE, SITE_URL + 'callback')
-		# redirect to facebook to get your code
-		redirect session['oauth'].url_for_oauth_code(:permissions => "publish_stream")
-		# redirect session['oauth'].url_for_oauth_code()
+		redirect session['oauth'].url_for_oauth_code(:permissions => "publish_stream")		
 	end
 
 	get '/logout' do
@@ -89,42 +75,45 @@ class CHESSMASTER < Sinatra::Application
 	end
 
 	get '/games' do
-
 		@games = Game.all(:order => :item.asc)
-		#session['games'] = @games
-		#if session['access_token']		
-		erb :games
-		#else
-  			#redirect '/login'
-  		#end
+		if session['access_token']		
+			erb :games
+		else
+  			redirect '/login'
+  		end
 	end
 
-	get '/mygames' do		
-		#session['games'] = @games
-		if session['access_token']	
-
-			@player1Id = session['userId']
-			@games = Game.where(:player1Id => @player1Id).order(:item)
-
-			erb :games
+	get '/mygames' do
+		if session['access_token']
+			
+			@games = Game.where(:player1Id => session['userId']).order(:item)
+			erb :mygames
 
 		else
   			redirect '/login'
   		end
 	end
 
+	get '/mygamenew' do	
+		if session['access_token']	
+			userInfo = session['userInfo']		
+			game = chessmasterbo.newgame(userInfo)
+			chessmasterbo.loadchessboardwhite(game.gameId, userInfo)
+			@games = Game.where(:player1Id => userInfo['id']).order(:item)
+			erb :mygames
+		else
+  			redirect '/login'
+  		end
+	end
+
 	get '/chessboard' do
-		#if session['access_token']
-			erb :chessboard
-		#else
-			#redirect '/login'
-		#end
-  		
+		erb :chessboard	
 	end
 
 	get '/see' do
 		if session['access_token']	
-			@gameId = params[:game]
+			@gameId = params[:gameId]
+			@userInfo = session['userInfo']
 			@game = Game.where(:gameId => @gameId).first
 			erb :chessboard
 		else
@@ -134,24 +123,8 @@ class CHESSMASTER < Sinatra::Application
 	end
 
 	get '/play' do
-		@gameId		= params[:gameId]
-		@piece		= params[:piece]
-		@origin		= params[:origin]
-		@final		= params[:final]
-		@playerId 	= session['userId']
-
-		chessboard = Chessboard.where(:gameId => @gameId, :playerId => @playerId, :piece => @piece).first
-
-		chessboard.update_attributes(
-				:origin			=> @origin,
-				:final			=> @final,
-				:lastModified	=> Time.now.to_s
-			)
-		
-		result = Chessboard.where(:gameId => @gameId)
-
-		return result.to_json
-
+		result = chessmasterbo.updatechessboard(params[:gameId], params[:piece], params[:origin], params[:final])		
+		return result# chessmasterbo.writelisttojson(result).to_json #result.to_json
 	end
 
 	get '/about' do
@@ -159,70 +132,31 @@ class CHESSMASTER < Sinatra::Application
 	end
 
 	get '/new' do
-		uuid = UUID.new
-		game = Game.new
-
-		game.item				= Game.count + 1
-		game.gameId	 			= uuid.generate
-		game.player1 			= session['name']
-		game.player1Id			= session['userId']
-		game.player1UserName	= session['username']
-		game.player1Email		= session['email']
-		game.creationDate		= Time.now.to_s
-		game.status				= 'New'
-		game.url     			= 'http://fb-chess.herokuapp.com/see?game=' +  game.gameId
-
-		
-		if game.save
-			status 201
+		if session['access_token']	
+			userInfo = session['userInfo']		
+			game = chessmasterbo.newgame(userInfo)
+			chessmasterbo.loadchessboardwhite(game.gameId, userInfo)
+			# publish Facebook
+			chessmasterbo.putwallpost(session["access_token"], game.player1 + " has created a chess game.! " + game.url)
+			@games = Game.all(:order => :item.asc)
+			erb :games
 		else
-			status 401
-		end
-
-		# Load chessboard 
-		chessboard = Chessboard.new
-
-		chessboard.gameId		= game.gameId
-		chessboard.playerId		= session['userId']
-		chessboard.piece		= 'BlackHose'
-		chessboard.origin		= 'A2'
-		chessboard.final		= 'C1'
-
-		# Facebook
-		@graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-		@graph.put_wall_post(game.player1 + " has created a chess game.! " + game.url )
-
-		if chessboard.save
-			status 201
-		else
-			status 401
-		end
-
-		@games = Game.all(:order => :item.asc)
-
-		erb :games
+  			redirect '/login'
+  		end
 	end
 
 	get '/challenge' do
-		@gameId = params[:game]
-		game = Game.where(:gameId => @gameId).first
-
-		game.update_attributes(
-			:player2 => 'Jose Perez',
-			:currentPlayer => game.player1,
-			:currentPlayerId => game.player1Id,
-			:status => 'In Progress'
-			)
-
+		gameId = params[:gameId]
+		chessmasterbo.challenge(gameId, session['userInfo'])
+		chessmasterbo.loadchessboardblack(gameId, session['userInfo'])
 		@games = Game.all(:order => :item.asc)
-
 		erb :games
 	end
 
 	get '/deleteall' do
 		Game.destroy_all
+		Chessboard.destroy_all
 		@games = Game.all
-
 		erb :games
 	end
 
---
+++ lib/chessmasterbo.rb
@@ -0,0 +1,124 @@
+require 'rubygems'
+require 'sinatra'
+require 'koala'
+require 'mongo'
+require 'json'
+require 'mongo_mapper'
+require 'uri'
+
+class Chessmasterbo
+	
+	enable :sessions
+
+	def newgame(userInfo)
+		
+		uuid = UUID.new
+		game = Game.new
+
+		game.item				= Game.count + 1
+		game.gameId	 			= uuid.generate
+		game.player1 			= userInfo['name']
+		game.player1Id			= userInfo['Id']
+		game.player1UserName	= userInfo['username']
+		game.player1Email		= userInfo['email']
+		game.creationDate		= Time.now.to_s
+		game.status				= 'New'
+		game.url     			= 'http://fb-chess.herokuapp.com/see?game=' +  game.gameId		
+		game.save
+
+		return game
+
+	end
+
+	def loadchessboardwhite(gameId, userInfo)
+		# Load chessboard 
+		savepiece(gameId, 'WhiteTower',		'A1', 'A1', userInfo)
+		savepiece(gameId, 'WhiteHorse',		'B1', 'B1', userInfo)
+		savepiece(gameId, 'WhiteBishop',	'C1', 'C1', userInfo)
+		savepiece(gameId, 'WhiteQueen',		'D1', 'D1', userInfo)
+		savepiece(gameId, 'WhiteKing',		'E1', 'E1', userInfo)
+		savepiece(gameId, 'WhiteBishop',	'F1', 'F1', userInfo)
+		savepiece(gameId, 'WhiteHorse',		'G1', 'G1', userInfo)
+		savepiece(gameId, 'WhiteTower', 	'H1', 'H1', userInfo)
+		savepiece(gameId, 'WhitePawn',		'A2', 'A2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'B2', 'B2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'C2', 'C2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'D2', 'D2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'E2', 'E2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'F2', 'F2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'G2', 'G2', userInfo)
+		savepiece(gameId, 'WhitePawn',		'H2', 'H2', userInfo)
+	end
+
+	def loadchessboardblack(gameId, userInfo)
+		savepiece(gameId, 'BlackTower', 	'A8', 'A8', userInfo)
+		savepiece(gameId, 'BlackHorse', 	'B8', 'B8', userInfo)
+		savepiece(gameId, 'BlackBishop',	'C8', 'C8', userInfo)
+		savepiece(gameId, 'BlackQueen', 	'D8', 'D8', userInfo)
+		savepiece(gameId, 'BlackKing', 		'E8', 'E8', userInfo)
+		savepiece(gameId, 'BlackBishop', 	'F8', 'F8', userInfo)
+		savepiece(gameId, 'BlackHorse', 	'G8', 'G8', userInfo)
+		savepiece(gameId, 'BlackTower', 	'H8', 'H8', userInfo)
+		savepiece(gameId, 'BlackPawn',		'A7', 'A7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'B7', 'B7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'C7', 'C7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'D7', 'D7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'E7', 'E7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'F7', 'F7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'G7', 'G7', userInfo)
+		savepiece(gameId, 'BlackPawn',		'H7', 'H7', userInfo)
+	end
+
+	def updatechessboard(gameId, piece, origin, final)
+
+		chessboard = Chessboard.where(:gameId => gameId, :piece => piece, :final => origin).first
+
+		chessboard.update_attributes(
+				:origin			=> origin,
+				:final			=> final,
+				:lastModified	=> Time.now.to_s
+			)
+		
+		olist = Chessboard.all(:order => :item.asc)
+
+		return writelisttojson(olist)
+	end
+
+	def savepiece(gameId, piece, initial, final, userInfo)
+		chessboard = Chessboard.new
+		chessboard.gameId		= gameId
+		chessboard.playerId		= userInfo['id']
+		chessboard.piece		= piece
+		chessboard.origin		= initial
+		chessboard.final		= final
+		chessboard.save
+	end
+
+	def challenge(gameId, userInfo)
+		game = Game.where(:gameId => gameId).first
+		game.update_attributes(
+			:player2 			=> userInfo['name'],
+			:player2Id 			=> userInfo['id'],
+			:player2UserName	=> userInfo['username'],
+			:player2Email 		=> userInfo['email'],
+			:currentPlayer 		=> game.player1,
+			:currentPlayerId 	=> game.player1Id,
+			:status 			=> 'In Progress'
+			)
+	end
+
+	def putwallpost(access_token, message)
+		@graph = Koala::Facebook::GraphAPI.new(access_token)
+		@graph.put_wall_post(message)
+	end
+
+
+	def writelisttojson(olist)
+		result = ''
+		olist.each do |item|
+			result = result + item.to_json.to_s
+		end
+		return result
+	end
+
+end
